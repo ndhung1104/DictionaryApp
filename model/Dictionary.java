912adf50174887dca9a0b0199c3cb27bee64a93a
ndhung1104 <ndhung1104@gmail.com>
2025-11-07 09:03:03 +0700

    Add Dictionary and Entry classes with basic structure

---
+++ model/Dictionary.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+    private List<Entry> wordsList = new ArrayList<>();
+}
---
+++ model/Entry.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Entry {
+    private String word;
+    private String definition;
+
+    public Entry(String word, String definition) {
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
